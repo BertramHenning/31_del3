e957862bedd91eb132634300146041acd615349c
Bertram <Bertram@Bertram-HP.eduroam.wireless.dtu.dk>
2016-11-17 13:40:47 +0100

    Added Player class

--- 31_del3/src/entity/Player.java
+++ 31_del3/src/entity/Player.java
@@ -2,4 +2,36 @@ package entity;
 
 public class Player {
 
+	private int current;
+	private String name;
+
+	private Account points;
+
+	public Player(String a) {
+		name = a;
+		points = new Account();
+	}
+	
+	// Adds score to Account object
+	public void addPoints(int a) {
+		points.addCoins(a);
+	}
+
+	// Gets score from Account object
+	public int getPoints() {
+		return points.getCoins();
+	}
+
+	public int getCurrent() {
+		return current;
+	}
+
+	public void setCurrent(int current) {
+		this.current = current;
+	}
+
+	public String toString() {
+		return name;
+	}
+
 }
