ed0fb7756eed6abe87ba216a1f929c5d42156f81
Bertram <Bertram@Bertram-HP.eduroam.wireless.dtu.dk>
2016-11-17 16:08:18 +0100

    Created list of Fields

--- 31_del3/src/entity/GameBoard.java
+++ 31_del3/src/entity/GameBoard.java
@@ -1,5 +1,46 @@
 package entity;
 
+import java.util.Arrays;
+
+import boundary.Descriptions;
+
 public class GameBoard {
+	Field[] fields = new Field[21];
+	
+	public GameBoard(){
+		for (int i = 0; i < 21; i++){
+			switch(i){
+			case 3:
+			case 7:
+			case 13:
+			case 17:
+				fields[i] = new Fleet(1);
+				break;
+			case 5:
+			case 15:
+				fields[i] = new Tax(1);
+				break;
+			case 8:
+			case 18:
+				fields[i] = new LaborCamp(1, 1);
+				break;
+			case 10:
+			case 20:
+				fields[i] = new Refuge(1);
+				break;
+			default:
+				fields[i] = new Territory(1, 1);
+				break;
+			}
+		}
+	}
+
+	public String toString() {
+		return "GameBoard [fields=" + Arrays.toString(fields) + "] \n";
+	}
+	
+	
+	
+	
 
 }
