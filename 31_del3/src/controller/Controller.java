6b23b437fdc6305e38f57baa9b2374482d03ee75
Bertram <Bertram@Bertram-HP.eduroam.wireless.dtu.dk>
2016-11-23 15:09:42 +0100

    started adding game logic

--- 31_del3/src/controller/Controller.java
+++ 31_del3/src/controller/Controller.java
@@ -1,14 +1,51 @@
 package controller;
 
 import boundary.GUIHandler;
+import entity.DiceCup;
 import entity.GameBoard;
 import entity.PlayerList;
 
 public class Controller {
-	GUIHandler gui;
-	GameBoard board;
-	PlayerList list;
+	private GUIHandler gui;
+	private GameBoard board;
+	private PlayerList list;
+	private DiceCup cup;
+	private int turn = 0;
 	
+
+	public Controller() {
+		gui = new GUIHandler();
+		board = new GameBoard();
+		cup = new DiceCup(2);
+	}
+
+	public void startGame(){
+		gui.createBoard();
+		addPlayers();
+		for ( int i = 0; i < list.getPlayerAmount(); i++){
+			System.out.println(i);
+			gui.addPlayer(list.getName(i));
+		}
+		while(true){
+			gui.showMessage("It's your turn " + list.getName(turn)+", press OK to roll dice.");
+			
+			cup.rollDice();
+			gui.setDice(cup.getFaceValue(0), cup.getFaceValue(1));
+			
+			list.movePosition(turn, cup.getSum());
+			gui.moveCar(list.getName(turn), list.getPosition(turn));
+			
+			break;
+		}
+		
+	}
 	
-	
+	public void addPlayers(){
+		int playerAmount = gui.askAmount("Enter player amount between 2 and 6:", 2, 6);
+		String[] names = new String[playerAmount];
+		for (int i = 0; i < playerAmount; i++){
+			names[i] = gui.askString("Player "+ (i+1) + " enter your name");
+		}
+		list = new PlayerList(playerAmount, names);
+	}
 }
