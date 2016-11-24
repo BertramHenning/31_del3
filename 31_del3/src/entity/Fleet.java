d7545ec4077937808f3f9b680c06709bb8e0ebec
Bertram <Bertram@Bertram-HP>
2016-11-24 17:59:07 +0100

    Changes to landOnField

--- 31_del3/src/entity/Fleet.java
+++ 31_del3/src/entity/Fleet.java
@@ -1,5 +1,8 @@
 package entity;
 
+import boundary.Descriptions;
+import desktop_resources.GUI;
+
 public class Fleet extends Ownable {
 
 	private final int RENT_1 = 500;
@@ -12,16 +15,52 @@ public class Fleet extends Ownable {
 		super(price);
 		
 	}
+	
+	@Override
+	public void landOnField(Player player) {
+		if (owner == null && player.getCoins() > price) {
+			if (GUI.getUserLeftButtonPressed(Descriptions.fieldAsk[player.getPosition() - 1], "Yes", "No")) {
+				owner = player;
+				player.addCoins(-price);
+				player.setFleets(player.getFleets()+1);
+			}
+		} else if (owner == null) {
+			GUI.showMessage("You dont have enough coins to buy this fleet");
+		} else if (owner == player) {
+			GUI.showMessage("You have landed on your own fleet");
+		} else {
+			int rent;
+			switch(owner.getFleets()){
+			case 1:
+				rent = RENT_1;
+				break;
+			case 2:
+				rent = RENT_2;
+				break;
+			case 3:
+				rent = RENT_3;
+				break;
+			case 4:
+				rent = RENT_4;
+				break;
+			default:
+				rent = 0;
+			}
+			GUI.showMessage("You have landed on " + owner.toString() + "'s fleet and have to pay him " + rent + " coins");
+			if (player.getCoins() > rent) {
+				player.addCoins(-rent);
+				owner.addCoins(rent);
+			} else {
+				owner.addCoins(player.getCoins());
+				player.addCoins(-rent);
+			}
+		}
+	}
+
 	@Override
 	public int getRent() {
 		// TODO Auto-generated method stub
 		return 0;
-	}
-	
-	@Override
-	public void landOnField(Player player) {
-		// TODO Auto-generated method stub
-
 	}
 	
 	
--- 31_del3/src/entity/Tax.java
+++ 31_del3/src/entity/Tax.java
@@ -1,8 +1,11 @@
 package entity;
 
+import boundary.Descriptions;
+import desktop_resources.GUI;
+
 public class Tax extends Field {
 	private int taxAmount;
-	private int taxRate;
+	private int taxRate = 10;
 
 	public Tax(int taxAmount){
 		this.taxAmount = taxAmount;
@@ -10,7 +13,16 @@ public class Tax extends Field {
 	
 	@Override
 	public void landOnField(Player player) {
-		player.addCoins(taxAmount);
+		if (taxAmount == 2000){
+			GUI.showMessage(Descriptions.fieldDescription[player.getPosition()-1]);
+			player.addCoins(-taxAmount);
+		} else {
+			if (GUI.getUserLeftButtonPressed(Descriptions.fieldAsk[player.getPosition()-1], "" + taxAmount, "" + player.getCoins()/100*taxRate)){
+				player.addCoins(-taxAmount);	
+			} else {
+				player.addCoins(-player.getCoins()/100*taxRate);
+			}
+		}
 	
 	}
 
