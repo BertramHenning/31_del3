d06550efdea68f206056d4affbdada9dc982e985
Bertram <Bertram@Bertram-HP.eduroam.wireless.dtu.dk>
2016-11-24 11:26:06 +0100

    Created landOnField

--- 31_del3/src/entity/Territory.java
+++ 31_del3/src/entity/Territory.java
@@ -1,9 +1,11 @@
 package entity;
 
+import desktop_resources.GUI;
+
 public class Territory extends Ownable {
 	private int rent;
-	
-	public Territory(int price,int rent){
+
+	public Territory(int price, int rent) {
 		super(price);
 		this.rent = rent;
 	}
@@ -15,10 +17,25 @@ public class Territory extends Ownable {
 
 	@Override
 	public void landOnField(Player player) {
-		// TODO Auto-generated method stub
+		if (owner == null && player.getCoins() > price) {
+			if (GUI.getUserLeftButtonPressed("Do you want to purchase this territory for " + price + "?", "Yes", "No")) {
+				owner = player;
+				player.addCoins(-price);
+			}
+		} else if (owner == null) {
+			GUI.showMessage("You dont have enough coins to buy this territory");
+		} else {
+			GUI.showMessage(
+					"You have landed on " + owner.toString() + "'s territory and have to pay him " + rent + "coins");
+			if (player.getCoins() > rent) {
+				player.addCoins(-rent);
+				owner.addCoins(rent);
+			} else {
+				owner.addCoins(player.getCoins());
+				player.addCoins(-rent);
+			}
+		}
 
 	}
 
-	
-	
 }
